4bbfaccf6f0a9d5e68db5d69642901b7c9103740
Hasan <h.hasanbayli@xalqsigorta.az>
2022-03-30 15:52:15 +0400

    Add IdentityServer empty project

---
+++ IdentityServer/FreeCourse.IdentityServer/Program.cs
@@ -0,0 +1,26 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+IServiceCollection services = builder.Services;
+IConfiguration configuration = builder.Configuration;
+// Add services to the container.
+
+services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
