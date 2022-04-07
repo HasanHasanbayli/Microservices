ffa3614fa9f997cf8956e6acd17b46fe1a5e1eb9
Hasan <h.hasanbayli@xalqsigorta.az>
2022-04-07 17:27:26 +0400

    Add Basket Api Dtos

---
+++ Services/Basket/FreeCourse.Services.Basket/DTOs/BasketDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeCourse.Services.Basket.DTOs;
+
+public class BasketDto
+{
+    public string UserId { get; set; }
+    public string DiscountCode { get; set; }
+    public List<BasketItemDto> BasketItem { get; set; }
+
+    public decimal TotalPrice
+    {
+        get => BasketItem.Sum(x => x.Price * x.Quantity);
+    }
+}
---
+++ Services/Basket/FreeCourse.Services.Basket/DTOs/BasketItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeCourse.Services.Basket.DTOs;
+
+public class BasketItemDto
+{
+    public int Quantity { get; set; }
+    public string CourseId { get; set; }
+    public string CourseName { get; set; }
+    public decimal Price { get; set; }
+}
