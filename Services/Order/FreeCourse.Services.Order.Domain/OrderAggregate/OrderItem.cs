e324c8ed92177ca5ffa81ed81eb958addde27277
hasanhasanbayli <hasanhasanbeyli@gmail.com>
2022-04-15 16:24:08 +0400

    Add OrderDbContext

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,27 @@
+using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate;
+
+public class OrderItem : Entity
+{
+    public string ProductId { get; private set; }
+    public string ProductName { get; private set; }
+    public string PictureUrl { get; private set; }
+    public decimal Price { get; private set; }
+    public int Type { get; set; }
+
+    public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+    {
+        ProductId = productId;
+        ProductName = productName;
+        PictureUrl = pictureUrl;
+        Price = price;
+    }
+
+    public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+    {
+        ProductName = productName;
+        PictureUrl = pictureUrl;
+        Price = price;
+    }
+}
