67d01b21e29163aaa477461c4174a0fcd25b9f85
hasanhasanbayli <hasanhasanbeyli@gmail.com>
2022-05-05 21:25:52 +0400

    Add MessageCommand

---
+++ Shared/FreeCourse.Shared/Messages/OrderItem.cs
@@ -0,0 +1,12 @@
+namespace FreeCourse.Shared.Messages;
+
+public class OrderItem
+{
+    public string ProductId { get; set; }
+
+    public string ProductName { get; set; }
+
+    public string PictureUrl { get; set; }
+
+    public decimal Price { get; set; }
+}
